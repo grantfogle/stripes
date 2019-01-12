f5cf599802cbb80781637891988f97e0bb23bd51
grantfogle <grantjfogle@gmail.com>
2019-01-12 16:54:41 -0700

    set up dynamic generation and handle click function for tags

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,10 +1,8 @@
 import React, { Component } from 'React';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import { connect } from 'react-redux';
-import DrillCard from './DrillCard';
 import Card from './Card';
 import CardSection from './CardSection';
-import Tag from './Tag';
 import Button from './Button';
 import { Actions } from 'react-native-router-flux';
 
@@ -25,17 +23,42 @@ class Main extends Component {
                     name: 'Partner',
                     status: false,
                 },
-                fullGuard:
+                fullGuard: {
                     name: 'Full Guard',
-                status: false,
-                backControl: false,
-                halfGuard: false,
-                sideControl: false,
-                fullMount: false,
-                takedown: false,
-                escape: false,
-                sweep: false,
-                submission: false
+                    status: false,
+                },
+                backControl: {
+                    name: 'Back Control',
+                    status: false,
+                },
+                halfGuard: {
+                    name: 'Half Guard',
+                    status: false,
+                },
+                sideControl: {
+                    name: 'Side Control',
+                    status: false,
+                },
+                fullMount: {
+                    name: 'Full Mount',
+                    status: false,
+                },
+                takedown: {
+                    name: 'Takedown',
+                    status: false,
+                },
+                escape: {
+                    name: 'Escape',
+                    status: false,
+                },
+                sweep: {
+                    name: 'Sweep',
+                    status: false,
+                },
+                submission: {
+                    name: 'Submission',
+                    status: false
+                },
             }
         }
     }
@@ -43,37 +66,24 @@ class Main extends Component {
     handleClick = (name) => {
         this.setState((prevState) => {
             let filterOptions = Object.assign({}, prevState.filterOptions)
-            filterOptions[name] = !prevState.filterOptions[name];
+            filterOptions[name].status = !prevState.filterOptions[name].status;
             return { filterOptions };
         })
     }
 
     generateDrillTags = () => {
-        const drillTags = this.state.filterOptions.warmup;
-        console.log(drillTags)
+        const drillTags = this.state.filterOptions;
+        const drills = [];
         for (let prop in drillTags) {
-            //onPress={this.handleClick(name)}
-            console.log(prop)
-            console.log(drillTags.name)
-            return (
-                <TouchableOpacity style={styles.clickedStyle}>
-                    <Text style={styles.textStyle}>{drillTags.name}</Text>
-                </TouchableOpacity>
+            drills.push(
+                <TouchableOpacity style={drillTags[prop].status ? styles.clickedStyle : styles.unclickedStyle} onPress={() => this.handleClick(prop)}>
+                    <Text style={styles.textStyle}>{drillTags[prop].name}</Text>
+                </TouchableOpacity >
             )
+
         }
-        // style={styles.tagStyle}
-        // onPress = {() => {
-        //     this.handleClick(drill.name)
-        //     handleClick(name)
-        // }
-    }
-    // style = { s }
-    //     >
-    //     <Text style={styles.textStyle}>{children}</Text>
+        return drills;
 
-
-    generateDrillTag() {
-        return this.state.drills.map(drill => (<Tag title="drills" name={drill.name} clicked={drill.clicked} handleClick={this.handleClick}>{drill.name}</Tag>))
     }
 
     render() {
@@ -83,76 +93,14 @@ class Main extends Component {
                     <Text style={styles.headerText}>What do you feel like training today?</Text>
                 </View>
                 <View style={styles.content}>
-                    <View style={styles.filterTags}>
-                        <View style={styles.filterTagsRow}>
-                            {this.generateDrillTags()}
-                            <Tag name="warmup"
-                                clicked={this.state.filterOptions.warmup}
-                                handleClick={this.handleClick} >
-                                Warmup
-                            </Tag>
-                            <Tag name="solo"
-                                clicked={this.state.filterOptions.solo}
-                                handleClick={this.handleClick} >
-                                Solo
-                            </Tag>
-                            <Tag name="partner"
-                                clicked={this.state.filterOptions.partner}
-                                handleClick={this.handleClick}>
-                                Partner
-                            </Tag>
-                        </View>
-                        <View style={styles.filterTagsRow}>
-                            <Tag name="fullGuard"
-                                clicked={this.state.filterOptions.fullGuard}
-                                handleClick={this.handleClick}>
-                                Full Guard
-                            </Tag>
-                            <Tag name="backControl"
-                                clicked={this.state.filterOptions.backControl}
-                                handleClick={this.handleClick}>
-                                Back Control
-                            </Tag>
-                            <Tag name="halfGuard"
-                                clicked={this.state.filterOptions.halfGuard}
-                                handleClick={this.handleClick}>
-                                Half Guard
-                            </Tag>
-                            <Tag name="sideControl"
-                                clicked={this.state.filterOptions.sideControl}
-                                handleClick={this.handleClick}>
-                                Side Control
-                            </Tag>
-                            <Tag name="fullMount"
-                                clicked={this.state.filterOptions.fullMount}
-                                handleClick={this.handleClick}>
-                                Full Mount
-                            </Tag>
-                            <Tag name="takedown"
-                                clicked={this.state.filterOptions.takedown}
-                                handleClick={this.handleClick}>Takedowns</Tag>
-                        </View>
-                        <View style={styles.filterTagsRow}>
-                            <Tag name="escape"
-                                clicked={this.state.filterOptions.escape}
-                                handleClick={this.handleClick}>Escape</Tag>
-                            <Tag name="sweep"
-                                clicked={this.state.filterOptions.sweep}
-                                handleClick={this.handleClick}>
-                                Sweep
-                            </Tag>
-                            <Tag name="submission"
-                                clicked={this.state.filterOptions.submission}
-                                handleClick={this.handleClick}>Submissions</Tag>
-                        </View>
-                    </View>
-                    <Card>
-                        <CardSection>
-                            <Button route="drills" action={true} func={this.props.generateDrills}>Generate New Drills</Button>
-                        </CardSection>
-                    </Card>
+                    <View style={styles.filterTags}>{this.generateDrillTags()}</View>
                 </View>
-            </View >
+                <Card>
+                    <CardSection>
+                        <Button route="drills" action={true} func={this.props.generateDrills}>Generate New Drills</Button>
+                    </CardSection>
+                </Card>
+            </View>
         )
     }
 }
@@ -174,15 +122,11 @@ const styles = StyleSheet.create({
     },
     filterTags: {
         width: 100,
-        flexDirection: 'column',
+        flexDirection: 'row',
         width: '100%',
         flexWrap: 'wrap',
-        marginBottom: 40,
-    },
-    filterTagsRow: {
-        flexDirection: 'row',
         justifyContent: 'center',
-        flexWrap: 'wrap',
+        marginBottom: 40,
     },
     headerText: {
         fontSize: 32,
@@ -200,33 +144,43 @@ const styles = StyleSheet.create({
         alignItems: 'center'
     },
     unclickedStyle: {
+        backgroundColor: '#336699',
+        // backgroundColor: '#9b59b6',
+        borderRadius: 5,
+        borderWidth: 1,
+        borderColor: '#336699',
+        height: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginLeft: 10,
+        marginRight: 10,
+        marginTop: 10,
+        marginBottom: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
+    },
+    clickedStyle: {
         backgroundColor: '#9b59b6',
         borderRadius: 5,
         borderWidth: 1,
         borderColor: '#9b59b6',
+        height: 40,
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'center',
         marginLeft: 10,
         marginRight: 10,
         marginTop: 10,
         marginBottom: 10,
-        padding: 10,
-        opacity: .5,
-    },
-    clickedStyle: {
-        backgroundColor: '#336699',
-        borderRadius: 5,
-        borderWidth: 1,
-        borderColor: '#9b59b6',
-        marginLeft: 10,
-        marginRight: 10,
-        marginTop: 10,
-        marginBottom: 10,
-        padding: 10,
+        paddingLeft: 20,
+        paddingRight: 20,
         opacity: 1
     },
     textStyle: {
         alignSelf: 'center',
         color: '#f0f0f0',
-        fontSize: 24,
+        fontSize: 20,
         fontWeight: '400',
         paddingLeft: 5,
         paddingRight: 5,
