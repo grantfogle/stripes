c0667d8b12c2dba7cc3589dfec3aa7fa8f7df6b6
grantfogle <grantjfogle@gmail.com>
2019-01-12 14:41:02 -0700

    updating add drill component, updating drill function setup

--- src/components/CreateDrill.js
+++ src/components/CreateDrill.js
@@ -11,36 +11,86 @@ import Container from './Container';
 class CreateDrill extends Component {
     constructor(props) {
         super(props)
-        this.state = {}
+        this.state = {
+            name: '',
+            skill_level: '',
+            type: '',
+            solo: 'false',
+            reps: 10,
+            description: '',
+            // position: ''
+        }
+    }
+
+    //fetch request to database
+    onFormSubmit = () => {
+        fetch('localhost:3000/', {
+            method: 'POST',
+            body: JSON.stringify(this.state),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then(response => console.log(response.json));
+    }
+    //add into state?
+    updatingText = (text, label) => {
+        this.setState({ [label]: text })
     }
 
     render() {
+        console.log(this.state)
         return (
             <Container>
                 <Card>
-                    <Text style={styles.loginHeaderStyle} >Add a New Drill</Text>
+                    <Text style={styles.loginHeaderStyle}>Add a New Drill</Text>
                     <CardSection>
                         <Input
-                            label="name"
+                            label="Name"
+                            name="name"
+                            updatingText={this.updatingText}
                             placeholder="Drill Name"
                         />
                     </CardSection>
                     <CardSection>
                         <Input
-                            label="beltLevel"
+                            label="Solo"
+                            name="solo"
+                            updatingText={this.updatingText}
+                            placeholder="Partner or Solo?"
+                        />
+                    </CardSection>
+                    <CardSection>
+                        <Input
+                            label="Belt Level"
+                            name="skill_level"
+                            updatingText={this.updatingText}
                             placeholder="Current Belt Level"
                         />
                     </CardSection>
                     <CardSection>
                         <Input
-                            label="description"
+                            label="Description"
+                            name="description"
+                            updatingText={this.updatingText}
                             placeholder="Description"
                         />
                     </CardSection>
                     <CardSection>
                         <Input
-                            label="type"
-                            placeholder="Sub/Sweep/Escape"
+                            label="Type"
+                            name="type"
+                            updatingText={this.updatingText}
+                            placeholder="Warmup/Sub/Sweep/Escape"
+                        />
+                    </CardSection>
+                    <CardSection>
+                        <Input
+                            label="Position"
+                            name="position"
+                            // updatingText={this.updatingText}
+                            placeholder="Back control, guard, side control, etc."
                         />
                     </CardSection>
                     <CardSection>
